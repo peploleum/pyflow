55bc24340b8f2378c604bfdfe00da452f0111247
peploleum <peploleum@gmail.com>
2018-08-21 12:01:13 +0200

    fixed scripts. zk runs.

--- docker/zookeeper-kubernetes-cluster/build_env.bat
+++ docker/zookeeper-kubernetes-cluster/build_env.bat
@@ -2,5 +2,4 @@ echo "Building for environment" %1
 
 rd /s /q deployment
 
-
-REM vortex --template templates --output deployment -varpath .\environments\$1.yaml
+vortex --template templates --output deployment -varpath .\environments\%1.yaml
